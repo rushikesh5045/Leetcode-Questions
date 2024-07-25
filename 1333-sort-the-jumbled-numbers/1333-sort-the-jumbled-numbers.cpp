77442e805789bc8b3e16aefaa38c03b2cae5b1e3
Rushikesh Kadam <rushikeshkadam515@gmail.com>
2024-07-25 11:14:52 +0530

    Time: 190 ms (72.91%), Space: 123.4 MB (23.51%) - LeetHub

--- 1333-sort-the-jumbled-numbers/1333-sort-the-jumbled-numbers.cpp
+++ 1333-sort-the-jumbled-numbers/1333-sort-the-jumbled-numbers.cpp
@@ -1,29 +1,39 @@
 class Solution {
 public:
     vector<int> sortJumbled(vector<int>& mapping, vector<int>& nums) {
-        vector<pair<int, int>> storePairs;
-
-        for (int i = 0; i < nums.size(); ++i) {
-            int mappedValue = 0;
-            int temp = nums[i];
-            int place = 1;
-            if (temp == 0) {
-                storePairs.push_back({mapping[0], i});
-                continue;
-            }
-            while (temp != 0) {
-                mappedValue = place * mapping[temp % 10] + mappedValue;
-                place *= 10;
-                temp /= 10;
-            }
-            storePairs.push_back({mappedValue, i});
+        vector<string> st;
+        for(auto i:nums)
+        {
+            st.push_back(to_string(i));
         }
-
-        sort(storePairs.begin(), storePairs.end());
-        vector<int> answer;
-        for (auto pair : storePairs) {
-            answer.push_back(nums[pair.second]);
+        for(int i=0;i<st.size();i++)
+        {
+            int size = st[i].size();
+            for(int j=0;j<size;j++)
+            {
+                int a = st[i][j]-'0';
+                int b = mapping[a];
+                st[i][j]=b+'0';
+            }
         }
-        return answer;
+        vector<int> vec;
+        for(int i=0;i<st.size();i++)
+        {
+            vec.push_back(stoi(st[i]));
+        }
+        vector<pair<int,int>> temp;
+        for(int i=0;i<vec.size();i++)
+        {
+            temp.push_back({vec[i],nums[i]});
+        }
+        auto my =[](pair<int,int>p1,pair<int,int>p2){
+            return p1.first<p2.first;
+        };
+        sort(temp.begin(),temp.end(),my);
+        for(int i=0;i<nums.size();i++)
+        {
+            nums[i]=temp[i].second;
+        }
+        return nums;
     }
 };
