9be28c90b65e41321fbe3e435d07fcf2f39a1f45
Rushikesh Kadam <rushikeshkadam515@gmail.com>
2024-07-26 16:09:19 +0530

    Time: 57 ms (28.39%), Space: 48.8 MB (57.77%) - LeetHub

---
+++ 2813-to-be-or-not-to-be/2813-to-be-or-not-to-be.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return{
+         toBe:function(val1)
+    {
+
+            if(val1===val)
+            {
+                return true;
+            }
+            else{
+                throw new Error("Not Equal")
+            }
+    },
+     notToBe:function(val2)
+    {
+
+            if(val2!==val)
+            {
+                return true;
+            }
+            else{
+                throw new Error("Equal")
+            }
+    }
+    }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
