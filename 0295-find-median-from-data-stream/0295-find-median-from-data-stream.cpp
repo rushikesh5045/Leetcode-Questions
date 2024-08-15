7fa4a5888f70db03fe0bfc409a9cb3aef8bf8309
Rushikesh Kadam <rushikeshkadam515@gmail.com>
2024-08-15 11:39:59 +0530

    Time: 234 ms (98.08%), Space: 121.9 MB (64.72%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,40 +1,36 @@
 class MedianFinder {
 public:
-     priority_queue<int> left_max_heap; //max heap
-    priority_queue<int, vector<int>, greater<int>> right_min_heap; //min heap
-    MedianFinder() {
-        
-    }
-    
+    priority_queue<int> left_max_heap;
+    priority_queue<int, vector<int>, greater<int>> right_min_heap;
+    MedianFinder() {}
+
     void addNum(int num) {
-        if(left_max_heap.empty() || num < left_max_heap.top()) {
+        if(left_max_heap.empty()||num<left_max_heap.top())
+        {
             left_max_heap.push(num);
-        } else {
+        }else{
             right_min_heap.push(num);
         }
-        
-        
-        //always maintain left_max_heap size one greater than rigfht_min_heap size
-        //ya fir, dono ka size equal ho
-        
-        if(abs((int)left_max_heap.size() - (int)right_min_heap.size()) > 1) {
+
+        // mainting difference of 1 in left and right;
+        // left size must be greater
+
+        if(abs((int)left_max_heap.size()-(int)right_min_heap.size())>1)
+        {
             right_min_heap.push(left_max_heap.top());
             left_max_heap.pop();
-        } else if(left_max_heap.size() < right_min_heap.size()) {
+        }else if(left_max_heap.size()<right_min_heap.size())
+        {
             left_max_heap.push(right_min_heap.top());
             right_min_heap.pop();
         }
-        
     }
-    
+
     double findMedian() {
-        if(left_max_heap.size() == right_min_heap.size()) {
-            // matlab even number of elements hue honge
-            
-            return (double)(left_max_heap.top()+right_min_heap.top())/2;
+        if(left_max_heap.size()==right_min_heap.size())
+        {
+            return (double)(right_min_heap.top()+left_max_heap.top())/2;
         }
-        
-        //else hamare paas odd number of elemenes hue honge
         return left_max_heap.top();
     }
 };
