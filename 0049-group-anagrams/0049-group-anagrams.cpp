f4f14275a9b58fae131075d8dae06b6ec3517c8f
Rushikesh Kadam <rushikeshkadam515@gmail.com>
2024-08-26 09:02:35 +0530

    Time: 67 ms (9.88%), Space: 33.4 MB (8.44%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,18 +1,45 @@
+// class Solution {
+// public:
+//     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+//         unordered_map<string,vector<string>> mp;
+//         vector<vector<string>> ans;
+//         for(string str:strs)
+//         {
+//             string temp = str;
+//             sort(temp.begin(),temp.end());
+//             mp[temp].push_back(str);
+//         }
+//         for(auto it : mp)
+//         {
+//             ans.push_back(it.second);
+//         }
+//        return ans;
+//     }
+// };
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string,vector<string>> mp;
-        vector<vector<string>> ans;
-        for(string str:strs)
-        {
-            string temp = str;
-            sort(temp.begin(),temp.end());
-            mp[temp].push_back(str);
+        unordered_map<string, vector<string>> ans;
+
+        for (auto& s : strs) {
+            vector<int> count(26, 0);
+            for (char c : s) {
+                count[c - 'a']++;
+            }
+
+            string key;
+            for (int i : count) {
+                key += "#" + to_string(i);
+            }
+
+            ans[key].push_back(s);
         }
-        for(auto it : mp)
-        {
-            ans.push_back(it.second);
+
+        vector<vector<string>> result;
+        for (auto& pair : ans) {
+            result.push_back(pair.second);
         }
-       return ans;
+
+        return result;
     }
 };
