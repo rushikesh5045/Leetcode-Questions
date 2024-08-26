a810a299a34efb78bb663f61a227e36bd301bc86
Rushikesh Kadam <rushikeshkadam515@gmail.com>
2024-08-26 13:22:35 +0530

    Time: 991 ms (9.4%), Space: 74.7 MB (30.21%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,25 +1,46 @@
+// class Solution {
+// public:
+//     int longestConsecutive(vector<int>& nums) {
+//         if (nums.empty()) return 0;  
+        
+//         sort(nums.begin(), nums.end());
+//         int n = nums.size();
+//         int ans = 1;  
+//         int count = 1;  
+
+//         for (int i = 1; i < n; i++) {
+//             if (nums[i] == nums[i - 1]) {
+//                 continue;
+//             }
+//             if (nums[i] - nums[i - 1] == 1) {
+//                 count++;
+//             } else {
+//                 ans = max(ans, count);  
+//                 count = 1;  
+//             }
+//         }
+//         ans = max(ans, count);  
+//         return ans;
+//     }
+// };
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        if (nums.empty()) return 0;  
-        
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
-        int ans = 1;  
-        int count = 1;  
+        unordered_set<int>s(nums.begin(),nums.end());
+        int ans = 0;
 
-        for (int i = 1; i < n; i++) {
-            if (nums[i] == nums[i - 1]) {
-                continue;
-            }
-            if (nums[i] - nums[i - 1] == 1) {
-                count++;
-            } else {
-                ans = max(ans, count);  
-                count = 1;  
+        for(auto n:nums)
+        {
+            if(s.find(n-1)==s.end())
+            {
+                int l = 1;
+                while(s.find(n+l)!=s.end())
+                {
+                    l++;
+                }
+                ans = max(ans,l);
             }
         }
-        ans = max(ans, count);  
         return ans;
     }
 };
