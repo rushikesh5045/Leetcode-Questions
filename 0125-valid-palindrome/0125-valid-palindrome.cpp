4659968b737a14b75395b35292a0f9d599af2138
Rushikesh Kadam <rushikeshkadam515@gmail.com>
2024-08-26 13:26:44 +0530

    Time: 11 ms (5.36%), Space: 9.6 MB (10.23%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string ans="";
+        for(int i=0;i<s.size();i++)
+        {
+            if((s[i]>=65&&s[i]<=90))
+            {
+                s[i]= s[i]-'A'+'a';
+                cout<<s[i]<<endl;
+                ans+=s[i];
+            }else if(s[i]>=97&&s[i]<=122){
+               ans+=s[i];
+            }else if(s[i]>=48&&s[i]<=57)
+            {
+                ans+=s[i];
+            }
+            else{
+                continue;
+            }
+        }
+       string t = ans;
+       reverse(t.begin(),t.end());
+       return ans==t;
+    }
+};
